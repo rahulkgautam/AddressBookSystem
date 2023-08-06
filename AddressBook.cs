fdf2d4b3fe9500355891b8bce4de8ca5db5d5568
rahulkgautam <rahulkgautam125@gmail.com>
2023-08-06 15:20:17 +0530

    Add Multiple Person Contact

--- AddressBook.cs
+++ AddressBook.cs
@@ -12,33 +12,25 @@ namespace AddressBookSystem
             {
                 Console.WriteLine("Enter contact details:");
                 Address contact = new Address();
-
                 Console.Write("First Name: ");
                 contact.FirstName = Console.ReadLine();
-
                 Console.Write("Last Name: ");
                 contact.LastName = Console.ReadLine();
-
                 Console.Write("Address: ");
                 contact.Addreses = Console.ReadLine();
-
                 Console.Write("City: ");
                 contact.City = Console.ReadLine();
-
                 Console.Write("State: ");
                 contact.State = Console.ReadLine();
-
                 Console.Write("Zip: ");
                 contact.Zip = Convert.ToInt32(Console.ReadLine());
-
                 Console.Write("Phone Number: ");
                 contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
-
                 Console.Write("Email: ");
                 contact.Email = Console.ReadLine();
-
                 contacts.Add(contact);
                 Console.WriteLine("Contact added successfully!");
+                AddNewContact(contacts);
             }
             catch (Exception ex)
             {
@@ -47,8 +39,11 @@ namespace AddressBookSystem
         }
         public void ViewContacts(List<Address> contacts)
         {
-            foreach (var contact in contacts)
-            {
+            if (contacts.Count == 0){
+                Console.WriteLine("First Add At list one contact....Empty");
+                return;
+            }
+            foreach (var contact in contacts){
                 Console.WriteLine($"First Name: {contact.FirstName},Last Name: {contact.LastName},Address: {contact.Addreses},City: {contact.City},State: {contact.State},Zip: {contact.Zip},Phone Number: {contact.PhoneNumber},Email: {contact.Email}");
                 
             }
@@ -56,11 +51,13 @@ namespace AddressBookSystem
         public void EditContact(List<Address> contacts)
         {
             try {
+                if (contacts.Count == 0) {
+                    Console.WriteLine("First Add At list one contact....Empty");
+                    return;
+                 }
                 Console.WriteLine("Enter Your First Name");
                 var firstName = Console.ReadLine();
-                if (contacts != null)
-                {
-
+                if (contacts != null){
                     Address contact = contacts.Find((a) => a.FirstName == firstName);
                     if (contact == null)
                     {
@@ -70,25 +67,18 @@ namespace AddressBookSystem
 
                     Console.Write("Last Name: ");
                     contact.LastName = Console.ReadLine();
-
                     Console.Write("Address: ");
                     contact.Addreses = Console.ReadLine();
-
                     Console.Write("City: ");
                     contact.City = Console.ReadLine();
-
                     Console.Write("State: ");
                     contact.State = Console.ReadLine();
-
                     Console.Write("Zip: ");
                     contact.Zip = Convert.ToInt32(Console.ReadLine());
-
                     Console.Write("Phone Number: ");
                     contact.PhoneNumber = Convert.ToInt64(Console.ReadLine());
-
                     Console.Write("Email: ");
                     contact.Email = Console.ReadLine();
-
                     Console.WriteLine("Contact Edited successfully!");
                 }
                 else
@@ -104,14 +94,15 @@ namespace AddressBookSystem
         {
             try
             {
+                if (contacts.Count == 0){
+                    Console.WriteLine("First Add At list one contact....Empty");
+                    return;
+                }
                 Console.WriteLine("Enter Your First Name");
                 var firstName = Console.ReadLine();
-                if (contacts != null)
-                {
-
+                if (contacts != null){
                     Address contact = contacts.Find((a) => a.FirstName == firstName);
-                    if (contact == null)
-                    {
+                    if (contact == null){
                         Console.WriteLine("Your FirstName No match!!! Enter please try Again");
                         return;
                     }
@@ -126,6 +117,13 @@ namespace AddressBookSystem
             {
                 Console.WriteLine("Please Enter Valid ");
             }
+        }
+        public void AddNewContact(List<Address> contacts)
+        {
+            Console.WriteLine("Do you want add new Contact Press(Y/N)");
+            string addNewContact = Console.ReadLine().ToLower();
+            if (addNewContact == "y")
+                AddContact(contacts);
         }
     }
 }
